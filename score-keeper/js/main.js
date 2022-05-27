77cf8455868d6d42333c7996bd557eeaf413962f
Carlos Gámez <hardpx@gmail.com>
2022-05-26 20:04:21 -0500

    added select input score and reset functionality

--- score-keeper/js/main.js
+++ score-keeper/js/main.js
@@ -5,6 +5,8 @@ const reset = document.querySelector('#reset');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
 
+const playToSelect = document.querySelector('#playTo');
+
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
@@ -15,6 +17,8 @@ function addPointP1(){
         p1Score += 1;
         if(p1Score === winningScore){
             isGameOver = true;
+            p1Display.classList.add('winner');
+            p2Display.classList.add('looser');
         }
         p1Display.innerText = p1Score;
     }
@@ -26,11 +30,30 @@ function addPointP2(){
         p2Score += 1;
         if(p2Score === winningScore){
             isGameOver = true;
+            p2Display.classList.add('winner');
+            p1Display.classList.add('looser');
         }
         p2Display.innerText = p2Score;
     }
 }
 
+function resetScore(){
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.innerText = p1Score;
+    p2Display.innerText = p2Score;
+    p1Display.classList.remove('winner', 'looser');
+    p2Display.classList.remove('winner', 'looser');
+}
+
+function changePlayToSelect(){
+    winningScore = parseInt(this.value);
+    resetScore();
+}
+
+playToSelect.addEventListener('change', changePlayToSelect)
+
 p1btn.addEventListener('click', addPointP1);
 p2btn.addEventListener('click', addPointP2);
-
+reset.addEventListener('click', resetScore);
