5c6f3a3802406461c938e344a8c940fbe0d206d8
Carlos Gámez <hardpx@gmail.com>
2022-05-27 13:53:43 -0500

    refactor and completed

--- score-keeper/js/main.js
+++ score-keeper/js/main.js
@@ -1,50 +1,53 @@
-const p1btn = document.querySelector('#p1-btn');
-const p2btn = document.querySelector('#p2-btn');
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1-btn'),
+    display: document.querySelector('#p1Display')
+}
+
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2-btn'),
+    display: document.querySelector('#p2Display')
+}
+
 const reset = document.querySelector('#reset');
-
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
-
 const playToSelect = document.querySelector('#playTo');
 
-let p1Score = 0;
-let p2Score = 0;
 let winningScore = 1;
 let isGameOver = false;
 
-function addPointP1(){
+function addPoint(player, oponent){
     if(!isGameOver){
-        p1Score += 1;
-        if(p1Score === winningScore){
+        player.score += 1;
+        if(player.score === winningScore){
             isGameOver = true;
-            p1Display.classList.add('has-text-success');
-            p2Display.classList.add('has-text-danger');
+            player.display.classList.add('has-text-success');
+            oponent.display.classList.add('has-text-danger');
+            player.button.disabled = true;
+            oponent.button.disabled = true;
         }
-        p1Display.innerText = p1Score;
-    }
-}
-
-
-function addPointP2(){
-    if(!isGameOver){
-        p2Score += 1;
-        if(p2Score === winningScore){
-            isGameOver = true;
-            p2Display.classList.add('has-text-success');
-            p1Display.classList.add('has-text-danger');
-        }
-        p2Display.innerText = p2Score;
+        player.display.innerText = player.score;
     }
 }
 
 function resetScore(){
     isGameOver = false;
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.innerText = p1Score;
-    p2Display.innerText = p2Score;
-    p1Display.classList.remove('has-text-success', 'has-text-danger');
-    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1.score = 0;
+    p2.score = 0;
+    p1.display.innerText = p1.score;
+    p2.display.innerText = p2.score;
+    p1.display.classList.remove('has-text-success', 'has-text-danger');
+    p2.display.classList.remove('has-text-success', 'has-text-danger');
+    p1.button.disabled = false;
+    p2.button.disabled = false;
+}
+
+function addPointP1(){
+    addPoint(p1, p2)
+}
+
+function addPointP2(){
+    addPoint(p2, p1)
 }
 
 function changePlayToSelect(){
@@ -54,6 +57,6 @@ function changePlayToSelect(){
 
 playToSelect.addEventListener('change', changePlayToSelect)
 
-p1btn.addEventListener('click', addPointP1);
-p2btn.addEventListener('click', addPointP2);
+p1.button.addEventListener('click', addPointP1);
+p2.button.addEventListener('click', addPointP2);
 reset.addEventListener('click', resetScore);
