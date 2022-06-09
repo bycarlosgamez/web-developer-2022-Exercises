1561f6ee2df2593e18810817545dea2f648de0d1
Carlos Gámez <hardpx@gmail.com>
2022-06-09 13:51:45 -0500

    refactored using a promise and eliminating callback

--- rainbow-backgorund/app.js
+++ rainbow-backgorund/app.js
@@ -1,20 +1,39 @@
-const delayedColorChange = (newColor, delay, doNext) => {
-    setTimeout(() => {
-        document.body.style.backgroundColor = newColor;
-        doNext && doNext();
-    }, delay)
+// const delayedColorChange = (newColor, delay, doNext) => {
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = newColor;
+//         doNext && doNext();
+//     }, delay)
+// }
+
+// delayedColorChange('red', 1000, () => {
+//     delayedColorChange('orange', 1000, () => {
+//         delayedColorChange('yellow', 1000, () => {
+//             delayedColorChange('green', 1000, () => {
+//                 delayedColorChange('blue', 1000, () => {
+//                     delayedColorChange('magenta', 1000, () => {
+    
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// });
+
+const delayedColorChange = (newColor, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = newColor;
+            resolve();
+        }, delay)
+    })
 }
 
-delayedColorChange('red', 1000, () => {
-    delayedColorChange('orange', 1000, () => {
-        delayedColorChange('yellow', 1000, () => {
-            delayedColorChange('green', 1000, () => {
-                delayedColorChange('blue', 1000, () => {
-                    delayedColorChange('magenta', 1000, () => {
-    
-                    })
-                })
-            })
-        })
-    })
-});
+
+delayedColorChange('red', 1000)
+    .then(() => delayedColorChange('red', 1000))
+    .then(() => delayedColorChange('orange', 1000))
+    .then(() => delayedColorChange('yellow', 1000))
+    .then(() => delayedColorChange('green', 1000))
+    .then(() => delayedColorChange('blue', 1000))
+    .then(() => delayedColorChange('indigo', 1000))
+    .then(() => delayedColorChange('violet', 1000))
